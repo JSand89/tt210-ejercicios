6f3543e658d8256133608560a5ba36cdcaf4d99a
Jsand89 <javier.esand@gmail.com>
2024-09-26 07:12:53 -0500

    subiendo los proyectos de practica rerealizados con la clase 201 de TT, los proyectos son claculadora numeros pares y ejercicios

---
+++ ejercicios/main.js
@@ -0,0 +1,33 @@
+// let listaMercado = ["pasta","papa criolla","helado","papa","carne","pollo"]
+
+// console.log(listaMercado)
+// console.log(listaMercado.length)
+
+// listaMercado.push("costillas de cerdo")
+// console.log(listaMercado)
+// // listaMercado.forEach((item,index) =>{
+// //     console.log(item +" " +index)
+// // })
+
+// let  fibo =[0,1,1,2,3,5,8,13,21]
+
+// fiboNuevo = fibo.map(elemento =>{
+//     return elemento*2
+// })
+// console.log(fibo)
+// console.log(fiboNuevo)
+
+console.log("Conseguir un huevo")
+console.log("Conseguir una sarten")
+console.log("Conseguir aceite o mantequilla")
+aceiteOMantequilla = "mantequilla"
+console.log("conseguir espatula")
+console.log("Colocar la sarten en la estufa")
+console.log("Encender la estufga a fuego medio")
+if(aceiteOMantequilla =="aceite"){
+    console.log("Añadir aceite a la sarten")
+} else if (aceiteOMantequilla =="mantequilla"){
+    console.log("Agregar mantequilla a la saten")
+} else{
+    console.log("Apagar la estufa y buscar aceite o mantequilla")
+}
