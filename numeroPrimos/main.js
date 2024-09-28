f2c0aafd0152139f49acd6fb060b12ab7757c9e9
Jsand89 <javier.esand@gmail.com>
2024-09-28 12:30:01 -0500

    subiendo primera iteracion del ejercicio de numeros primos

---
+++ numeroPrimos/main.js
@@ -0,0 +1,45 @@
+//traer los elementos del DOM
+entrada = document.getElementById("numero")
+btn = document.getElementById("btn")
+salida = document.getElementById("resultado")
+//Inicializando variables, van a esta altura para que todos las puedan usar
+let numero = 0
+//capturar los numeros desde el navegador
+entrada.addEventListener("input",(evento)=>{
+    numero = evento.target.value
+})
+
+function numeroPrimo(n){
+    let esPrimo = false
+    let cuenta =0
+    if(n == 1){
+        esPrimo = false
+    }else{
+        for (let i = 2; i<=n; i++){
+            if(n%i==0){
+                cuenta = cuenta + 1
+            }
+        }
+        if(cuenta==1){
+            esPrimo= true
+        }else{
+            esPrimo=false
+        }
+    }
+    return esPrimo
+}
+
+btn.addEventListener("click",()=>{
+    resultado = numeroPrimo(numero)
+    console.log(resultado)
+    let hijo = document.createElement("h3")
+    if(resultado){
+        hijo.innerHTML =`el  ${numero} es primo`
+    }else{
+        hijo.innerHTML =`el  ${numero} no es primo`
+    }
+    salida.appendChild(hijo)
+
+})
+
+
