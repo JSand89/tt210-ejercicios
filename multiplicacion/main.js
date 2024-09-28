068ee016f7bded4ba041df60ba06674be87c3066
Jsand89 <javier.esand@gmail.com>
2024-09-28 11:43:27 -0500

    subiendo el ejercicio de multiplicar dos numeros

---
+++ multiplicacion/main.js
@@ -0,0 +1,30 @@
+//traer los elementos del DOM
+entrada = document.getElementById("numero")
+otraEntrada = document.getElementById("otroNumero")
+btn = document.getElementById("btn")
+salida = document.getElementById("resultado")
+//Inicializando variables, van a esta altura para que todos las puedan usar
+let numero = 0
+let otroNumero = 0
+//capturar los numeros desde el navegador
+entrada.addEventListener("input",(evento)=>{
+    numero = evento.target.value
+})
+otraEntrada.addEventListener("input",(evento)=>{
+    otroNumero= evento.target.value
+})
+// creo la funcion de multiplicación
+function multiplicacion(parametro,otroParametro){
+    return parametro*otroParametro
+}
+//uso la funcion multiplicacion cuando le doy click al boton y creo el elemento h3 y lo agrego como hijo a salida
+btn.addEventListener("click",()=>{
+    resultado = multiplicacion(numero,otroNumero)
+    let hijo = document.createElement("h3")
+    hijo.innerHTML =`el resultado de ${numero}x${otroNumero} es ${resultado}`
+    salida.appendChild(hijo)
+})
+
+let resultado=multiplicacion(5,10)
+
+console.log(resultado)
